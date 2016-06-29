5bd0fd2c419abb3de6ec478c49dea3e42c16ae35
alxest <youngju.song@sf.snu.ac.kr>
2016-06-29 17:54:49 +0900

    Working Linearly

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -86,16 +86,23 @@ object LLVMBerryLogics {
 
   def make = ???
 
-  def generate(base: String) = {
+  def generate(ll_base: String) = {
     // cmd = "opt -stats #{OPT_OPTION} #{base}.ll -o #{base}.#{OUT_NAME}.ll -S 2>&1"
     // result = %x(zsh -c "#{cmd}")
     // x = [$?.success?? :generate_success : :generate_fail, cmd, result]
     // File.open("#{base}.result", 'w').write(result) #unless(x[0] == :generate_success)
-    val cmd = s"opt -stats $OPT_OPTION $base.ll -o $base.$OUT_NAME.ll -S"
+    val cmd = s"opt -stats $OPT_OPTION $ll_base.ll -o $ll_base.$OUT_NAME.ll -S"
     exec(cmd)
   }
 
-  def validate = ???
+  def validate(triple_base: String) = {
+    // result = %x(zsh -c "../ocaml_refact/main.native #{src} #{tgt} #{hint} 2>&1")
+    val hint = triple_base + ".hint.json"
+    val src = triple_base + ".src.bc"
+    val tgt = triple_base + ".tgt.bc"
+    val cmd = s"${SIMPLBERRY_DIR}/../ocaml_refact/main.native #{src} #{tgt} #{hint}"
+    exec(cmd)
+  }
 
   def which_opt = ???
 
@@ -122,8 +129,15 @@ object LLVMBerryLogics {
 object MainScript extends App {
   val GQ = new ConcurrentLinkedQueue[String]
   val VQ = new ConcurrentLinkedQueue[String]
-  val GQR = new AtomicReference(List[WorkResult]())
-  val VQR = new AtomicReference(List[WorkResult]())
+  // var GQ_processed = new AtomicInteger(0)
+  // var GQ_processed = 0
+
+  // val GQR = new AtomicReference(List[JobResult]())
+  // val VQR = new AtomicReference(List[JobResult]())
+  var GQR = scala.collection.mutable.Queue[JobResult]()
+  var VQR = scala.collection.mutable.Queue[JobResult]()
+  // listbuffer? arraybuffer?
+
   // val GQR = new ConcurrentLinkedQueue[WorkResult]()
   // val VQR = new ConcurrentLinkedQueue[WorkResult]()
 
@@ -134,56 +148,101 @@ object MainScript extends App {
       "/inputs_full"
   ).foreach(GQ.offer(_))
   println(GQ.size)
+  val GQ_total = GQ.size
+  var VQ_total = 0
 
-  sealed abstract class Work
-  case object ProcessGQ extends Work
-  case object ProcessVQ extends Work
-  case object Nothing extends Work
+  sealed abstract class Job
+  case class GQJob(val ll_base: String) extends Job
+  case class VQJob(val triple_base: String) extends Job
+  case object Nothing extends Job
+  case object Terminate extends Job
   // just Option Boolean?
 
-  class WorkResult (
+  class JobResult (
     val FileSize: Long,
     val Time: Double,
     val Generated: Option[Int]
   ) {} //without val, it is private
 
-  def decideNextWork: Work = {
-    println(GQ.size + " " + VQ.size)
-    val total_time_elapsed =
-      GQR.get.foldLeft(0.0)((s: Double, i: WorkResult) => s + i.Time)
-    println(total_time_elapsed)
-    ProcessGQ
+  var count = 0
+
+  def fetchNextJob: Job = {
+    MainScript.synchronized {
+      count += 1
+      if(count % 50 == 0) {
+        println("GQ : " + GQ.size + ", GQR : " + GQR.size + "/" + GQ_total)
+        println("VQ : " + VQ.size + ", VQR : " + VQR.size + "/" + VQ_total)
+      }
+      if(GQR.size == GQ_total && VQR.size == VQ_total) return Terminate
+
+      // println(GQR.get.size + " " + VQR.get.size)
+      // println("-----------------------------------------------")
+      // val GQ_total_time_elapsed =
+      //   this.synchronized {
+      //     println(GQR)
+      //     GQR.foldLeft(0.0)((s: Double, i: JobResult) => s + i.Time)
+      //   }
+
+      // println(total_time_elapsed)
+      val ret = Option(GQ.poll)
+      if(ret.isDefined) GQJob(ret.get)
+      else {
+        val ret2 = Option(VQ.poll)
+        if(ret2.isDefined) VQJob(ret2.get)
+        else Nothing
+      }
+    }
   }
 
-  def processGQ: Option[WorkResult] = {
-    for(ll_base <- Option(GQ.poll)) yield {
-      val t0 = System.currentTimeMillis
-      val FileSize = (new File(ll_base + ".ll")).length
+  def processGQ(ll_base: String): JobResult = {
+    val t0 = System.currentTimeMillis
+    val FileSize = (new File(ll_base + ".ll")).length
+    try {
       val res = LLVMBerryLogics.generate(ll_base)
       val tri_bases = LLVMBerryLogics.get_triple_bases(ll_base)
       if(tri_bases._1 == 0) {
         for(tri_base <- tri_bases._2.split("\n")) {
           VQ.offer(tri_base)
         }
+        MainScript.synchronized { VQ_total += tri_bases._2.split("\n").size }
       }
       val t1 = System.currentTimeMillis
-      new WorkResult(FileSize, (t1 - t0)/1000.0, None)
+      new JobResult(FileSize, (t1 - t0)/1000.0, None)
+    }
+    catch {
+      case e: Throwable =>
+        println("EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE " + e)
+        val t1 = System.currentTimeMillis
+        new JobResult(FileSize, (t1 - t0)/1000.0, None)
     }
   }
 
-  def processVQ: Option[WorkResult] = ???
+  def processVQ(triple_base: String): JobResult = {
+    val t0 = System.currentTimeMillis
+    val FileSize = (new File(triple_base + ".ll")).length
+    val res = LLVMBerryLogics.validate(triple_base)
+    println(res._1)
+    val t1 = System.currentTimeMillis
+    new JobResult(FileSize, (t1 - t0)/1000.0, None)
+  }
 
   class MyThread extends Thread {
     override def run {
       def runner: Unit = {
-        decideNextWork match {
-          case ProcessGQ =>
-            for(res <- processGQ)
-              GQR.set(res :: GQR.get) ; runner
-          case ProcessVQ =>
-            for(res <- processVQ)
-              VQR.set(res :: VQR.get) ; runner
-          case Nothing => ()
+        fetchNextJob match {
+          case GQJob(ll_base) =>
+            // GQ_processed.set(GQ_processed.get + 1)
+            val res = processGQ(ll_base)
+            MainScript.synchronized { GQR += res }
+            // this.synchronized { GQR += res }
+            runner
+          case VQJob(triple_base) =>
+            val res = processVQ(triple_base)
+            MainScript.synchronized { VQR += res }
+            // this.synchronized { VQR += res }
+            runner
+          case Nothing => () ; runner
+          case Terminate => ()
         }
       }
       runner
